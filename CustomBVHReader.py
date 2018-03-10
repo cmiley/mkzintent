4d39c3121ac431a3249e22e6f9273d51cb352ff8
Houston Lucas <houstonlucas@nevada.unr.edu>
2018-03-10 15:24:29 -0800

    Restructured the Dataset to load all data in memory to reduce I/O overhead during training.

--- CustomBVHReader.py
+++ CustomBVHReader.py
@@ -18,54 +18,38 @@ CONSTANTS
 '''#######################################################
 # Number of frames at the begining of the file that are invalid.
 NUM_INVALID_FRAMES = 1
+NUM_FRAMES_LOOK_AHEAD = 1
 NN_INPUT_SIZE = 93
 NN_OUTPUT_SIZE = 3
 
 class BVHDataset(data.Dataset):
 
     def __init__(self, file_paths):
-        self.length = 0
-        self.file_index_bounds = []
+        self.data = []
 
         for file_path in file_paths:
             with open(file_path) as f:
                 bvh_data = Bvh(f.read())
 
             # One extra frame is discarded at the end
-            num_samples = bvh_data.nframes - (NUM_INVALID_FRAMES + 1)
-            start_index = self.length
-            end_index = start_index + num_samples - 1
-            self.file_index_bounds.append( (file_path, start_index, end_index) )
-            # read length of bvh_data
-            self.length += num_samples
+            num_samples = bvh_data.nframes - (NUM_INVALID_FRAMES + NUM_FRAMES_LOOK_AHEAD)
+
+            for file_index in range(NUM_INVALID_FRAMES, num_samples - NUM_FRAMES_LOOK_AHEAD):
+                bvh_data.frames[file_index] = map(float, bvh_data.frames[file_index])
+                bvh_data.frames[file_index+NUM_FRAMES_LOOK_AHEAD] = map(float, bvh_data.frames[file_index+NUM_FRAMES_LOOK_AHEAD])
+
+                pose = np.asarray(bvh_data.frames[file_index][NN_OUTPUT_SIZE:])
+                initial = np.asarray(bvh_data.frames[file_index][:NN_OUTPUT_SIZE])
+                final = np.asarray(bvh_data.frames[file_index+NUM_FRAMES_LOOK_AHEAD][:NN_OUTPUT_SIZE])
+                delta = final - initial
+                self.data.append( (initial, pose, delta) )
+
+        self.length = len(self.data)
         
-
     def __getitem__(self, index):
-        # determine triple
-        item_triple = None
-
-        for item in self.file_index_bounds:
-            if index <= item[2]:
-                item_triple = item
-                break
-
-        # open file and pull index
-        fname = item_triple[0]
-        with open(fname) as f:
-            bvh_data = Bvh(f.read())
-
-        file_index = index - item_triple[1] + NUM_INVALID_FRAMES
-
-        bvh_data.frames[file_index] = map(float, bvh_data.frames[file_index])
-        bvh_data.frames[file_index+1] = map(float, bvh_data.frames[file_index+1])
-
-        found_frame = np.asarray(bvh_data.frames[file_index][NN_OUTPUT_SIZE:])
-        initial = np.asarray(bvh_data.frames[file_index][:NN_OUTPUT_SIZE])
-        final = np.asarray(bvh_data.frames[file_index+1][:NN_OUTPUT_SIZE])
-
-        delta = final - initial
+        positiion, pose, delta = self.data[index]
         observed_output_data = torch.from_numpy(delta).float()
-        input_data = torch.from_numpy(found_frame).float()
+        input_data = torch.from_numpy(pose).float()
 
         return input_data,observed_output_data
 
@@ -74,23 +58,23 @@ class BVHDataset(data.Dataset):
     
 def adjust_learning_rate(optimizer, epoch):
     """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
-    lr = (0.01 ** ((epoch+3) //3))
+    lr = (0.005 ** ((epoch+3) //3))
     for param_group in optimizer.param_groups:
         param_group['lr'] = lr
 
 
 def main():
     file_path_list = []
-    # for i in range(5):
-    #     for j in range(7,9):
-    #         file_path_list.append(
-    #             "bvh_testdata/bvh_conversion/cmu_bvh/{}/{}_{}.bvh".format(str(j).zfill(2), str(j).zfill(2), str(i+1).zfill(2))
-    #         )
+    for i in range(11):
+        for j in range(5,10):
+            file_path_list.append(
+                "bvh_testdata/bvh_conversion/cmu_bvh/{}/{}_{}.bvh".format(str(j).zfill(2), str(j).zfill(2), str(i+1).zfill(2))
+            )
 
-    for root, dirs, files in os.walk("bvh_testdata/bvh_conversion/cmu_bvh"):
-        for name in files:
-            if name.endswith(".bvh"):
-                file_path_list.append(os.path.join(root, name))
+    # for root, dirs, files in os.walk("bvh_testdata/bvh_conversion/cmu_bvh"):
+    #     for name in files:
+    #         if name.endswith(".bvh"):
+    #             file_path_list.append(os.path.join(root, name))
 
     print("Filenames loaded.")
     m_dataset = BVHDataset(file_path_list)
