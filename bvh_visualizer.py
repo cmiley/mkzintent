83b0be8ce6feb4d77028c42a5271500e70b93ec9
Houston Lucas <houstonlucas@nevada.unr.edu>
2018-04-04 16:14:44 -0700

    Added lookahead in vdata, Added green cube for actual position

--- bvh_visualizer.py
+++ bvh_visualizer.py
@@ -16,15 +16,41 @@ def main():
     with open(vdata_path, encoding='utf-8-sig') as file:
         lines = file.readlines()
 
+    num_frames = int(lines[0])
+    num_look_ahead = int(lines[1])
+    bvh_load_path = lines[2].strip()
+
+    data = []
+
+    for line in lines[3:]:
+        # X, Y, Z, dx, dy, dz = map(float, line.split(" "))
+        data.append(list(map(float, line.split(" "))))
+
+
+    # CUBE: Actual Position
+    bpy.ops.mesh.primitive_cube_add(radius=0.5, location=(0, 0, 0))
+    cube = bpy.context.object
+    cube.name = 'Cube'
+    bpy.context.scene.frame_current = 2
+    for i in range(len(data)-num_look_ahead):
+        cube.location.xyz = data[i+num_look_ahead][0], -data[i+num_look_ahead][2], data[i+num_look_ahead][1]
+        bpy.ops.anim.keyframe_insert_menu(type='Location')
+        bpy.context.scene.frame_current += 1
+
+    # Change it back to the actual for when we don't have a future position (near end of loop)
+    for i in range(len(data)-num_look_ahead, len(data)):
+        cube.location.xyz = data[i][0], -data[i][2], data[i][1]
+        bpy.ops.anim.keyframe_insert_menu(type='Location')
+        bpy.context.scene.frame_current += 1
+
+    # SPHERE: Predicted Position
     bpy.ops.mesh.primitive_ico_sphere_add(size=0.5, location=(0, 0, 0))
     sphere = bpy.context.object
     sphere.name = 'Sphere'
-    num_frames = int(lines[0])
-    bvh_load_path = lines[1].strip()
     bpy.context.scene.frame_current = 2
-    for line in lines[2:]:
-        X, Y, Z, dx, dy, dz = map(float, line.split(" "))
-        sphere.location.xyz = X + dx, -Z - dz, Y + dy
+
+    for i in range(len(data)):
+        sphere.location.xyz = data[i][0] + data[i][3], -data[i][2] - data[i][5], data[i][1] + data[i][4]
         bpy.ops.anim.keyframe_insert_menu(type='Location')
         bpy.context.scene.frame_current += 1
 
@@ -37,9 +63,13 @@ def main():
     bpy.context.scene.frame_end = num_frames + 2
     bpy.context.scene.frame_current = 0
 
-    # make target red
+    # make predicted red
     red = makeMaterial('Red', (1, 0, 0), (1, 1, 1), 1)
     setMaterial(sphere, red)
+
+    # make actual green
+    green = makeMaterial('Green', (0, 1, 0), (1, 1, 1), 1)
+    setMaterial(cube, green)
 
 
 def makeMaterial(name, diffuse, specular, alpha):
